5d498079e38169998df38daa8cd3dd1be056ce6f
Jameel <jameel.ahamed80@gmail.com>
2021-07-29 13:59:38 +0300

    telenoc enterprise theme

---
+++ telenoc_enterprise_theme/__manifest__.py
@@ -0,0 +1,14 @@
+{
+  'name':'Telenoc Enterprise Theme',
+  'description': 'This module for backend dashboard theme.',
+  'version': '14.0.1.0.0',
+  'category': 'Themes/Backend',
+  'author': 'Odoo Team, Telenoc',
+  'website': 'http://www.telenoc.org/',
+  
+    
+  'data': [ 'views/assets.xml'],
+    'images': [
+        'static/description/icon.png',
+        'static/description/banner.png'],
+}
